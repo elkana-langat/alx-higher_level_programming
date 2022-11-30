7788e96608e847d890e2d01b0a9e2c7b1c51c9a1
Elkana Lang'at <elkanahlangatt@gmail.com>
2022-11-30 09:43:28 +0000

    sec commit

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 
 def pow(a, b):
-    result = 1
-    if b > 0:
-        for i in range(0, b):
-            result *= a
-    elif b < 0:
-        for i in range(b, 0):
-            result /= a
-
-    return result
+    return a ** b
