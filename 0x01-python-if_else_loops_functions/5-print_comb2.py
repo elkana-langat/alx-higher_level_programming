d3e827b13e82bbd725ab65d2b27c953237378020
Elkana Lang'at <elkanahlangatt@gmail.com>
2022-11-30 05:42:39 +0000

    first commit

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(0, 100):
-    if i < 10:
-        print("0{} ,".format(i), end="")
-    if i > 9 and i < 99:
-        print("{} ,".format(i), end="")
     if i == 99:
-        print("99")
+        print("{}".format(i))
+    else:
+        print("{:02}".format(i), end=", ")
