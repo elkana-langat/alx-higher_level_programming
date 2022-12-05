eccd2e45b224d08160f9eefcda4dc26b2acf0b5d
Elkana Lang'at <elkanahlangatt@gmail.com>
2022-12-05 05:40:01 +0000

    Task 1 first commit

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    leng = len(my_list)
+
+    if idx < 0:
+        return (None)
+    elif idx > leng:
+        return (None)
+    else:
+        return (my_list[idx])
--- 0x03-python-data_structures/README.md
+++ 0x03-python-data_structures/README.md
@@ -3,4 +3,6 @@
 ## 0. Print a list of integers
 Write a lists that prints all integers of a list
 
+## 1. Secure access to an element in a list
+Write a function that retrieves an element from a list in C
 
