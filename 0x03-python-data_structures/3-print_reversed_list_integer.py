caa3bb41e5707c711bc8c2adaf175e451e44c595
Elkana Lang'at <elkanahlangatt@gmail.com>
2022-12-05 07:02:35 +0000

    Task 3 sec commit

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,5 +4,5 @@ def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
 
     leng = len(my_list)
-    for i in range(leng):
+    for i in range(0, leng):
         print("{:d}".format(my_list[i]))
