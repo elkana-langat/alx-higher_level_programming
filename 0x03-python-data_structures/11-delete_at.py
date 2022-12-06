c48e91f5daa7eccc1a61e74708974d6c0e52f179
Elkana Lang'at <elkanahlangatt@gmail.com>
2022-12-06 07:13:24 +0000

    Task 11 Fisrt commit

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    leng = len(my_list)
+
+    if idx < 0 or idx >= leng:
+        return (my_list)
+    else:
+        a = my_list[idx]
+        my_list.remove(a)
+
+        return (my_list)
