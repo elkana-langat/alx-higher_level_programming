df1d127dc651a01a05fd2df332467caa13ae0b6c
Elkana Lang'at <elkanahlangatt@gmail.com>
2022-12-19 13:36:14 +0000

    reduced code size Task 4

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,9 +3,8 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-        return (result)
     except (TypeError,ZeroDivisionError, ValueError):
         result = None
-        return (result)
     finally:
         print("Inside result: {}".format(result))
+    return (result)
