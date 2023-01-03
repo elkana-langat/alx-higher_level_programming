d061460e45ca5cd5672ff341d038b9452eb176d8
Elkana Lang'at <elkanahlangatt@gmail.com>
2023-01-03 15:48:53 +0000

    task 1 private instance

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,8 +12,8 @@ class Rectangle:
             width (int): The width of the Rectangle.
             height (int): The height of the Rectangle.
         """
-        self.__height = height
-        self.__width = width
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
