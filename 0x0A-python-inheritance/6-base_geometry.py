dc051ddc635d557ea49497cb45742fcad737ed8f
Elkana Lang'at <elkanahlangatt@gmail.com>
2023-01-09 14:13:03 +0000

    task 6 first commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Defines a method area which raises an exception."""
+    def area(self):
+        raise Exception("area() is not implimented")
