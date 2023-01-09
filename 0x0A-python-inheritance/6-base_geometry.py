addfde8ecb79935e2b96e607f32c84a0d015c86c
Elkana Lang'at <elkanahlangatt@gmail.com>
2023-01-09 14:14:52 +0000

    task 6 added docu

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -5,4 +5,5 @@
 class BaseGeometry:
     """Defines a method area which raises an exception."""
     def area(self):
+        """Not implimented raises an exception."""
         raise Exception("area() is not implimented")
