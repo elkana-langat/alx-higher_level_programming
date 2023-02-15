f0ec5a2f31ae78f02a1252298f20cfb786a9a460
Elkana Lang'at <elkanahlangatt@gmail.com>
2023-02-15 21:39:15 +0000

    fix err

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,6 @@
 -- creates a database and adds a user
 CREATE DATABASE
 	IF NOT EXISTS 'hbtn_0d_2';
-USE 'hbtn_0d_2';
 CREATE USER
 	IF NOT EXISTS 'user_0d_2'@'localhost'
 	IDENTIFIED BY 'user_0d_2_pwd';
