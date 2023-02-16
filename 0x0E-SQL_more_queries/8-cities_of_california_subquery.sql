a7fbcfd62217ad2549096eda0ec4b5a1e95b75af
Elkana Lang'at <elkanahlangatt@gmail.com>
2023-02-16 15:37:19 +0000

    task 8 first commit

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,5 @@
+-- create a script that lists all the cities of california
+-- that can be found in the database
+SELECT id, name FROM cities WHERE state_id IN
+	(SELECT id FROM states WHERE name = California)
+	ORDER BY id;
